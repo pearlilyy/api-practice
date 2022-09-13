98f8cffddc223ffbb081df65ab9c6f7f1007da87
Pearl <flairpearllee@gmail.com>
2022-09-13 12:37:42 -0700

    making boxoffice rank files by year

--- request/scrape.py
+++ request/scrape.py
@@ -1,9 +1,10 @@
+import os
 import datetime
 import requests
+import pandas as pd
 from requests_html import HTML
 
-now = datetime.datetime.now()
-year = now.year
+BASE_DIR = os.path.dirname(__file__)
 
 
 def url_to_txt(url, filename='world.html', save=False):
@@ -17,34 +18,53 @@ def url_to_txt(url, filename='world.html', save=False):
     return ""
 
 
-url = 'https://www.boxofficemojo.com/year/world/'
+def parse_and_extract(url, name='2022'):
+    html_text = url_to_txt(url)
 
-html_text = url_to_txt(url)
+    r_html = HTML(html=html_text)
+    table_class = ".imdb-scroll-table"
+    # table_class = "#table"
+    r_table = r_html.find(table_class)
 
-r_html = HTML(html=html_text)
-table_class = ".imdb-scroll-table"
-# table_class = "#table"
-r_table = r_html.find(table_class)
+    # print(r_table)
+    if len(r_table) == 1:
+        # print(r_table[0].text)
+        parsed_table = r_table[0]
+        rows = parsed_table.find('tr')
+        header_row = rows[0]
+        header_cols = header_row.find('th')
+        header_names = [x.text for x in header_cols]
+        table_data = []
+        for row in rows[1:]:
+            # print(row.text)
+            cols = row.find('td')
+            row_data = []
+            for i, col in enumerate(cols):
+                # print(i, col.text, '\n\n')
+                row_data.append(col.text)
+            table_data.append(row_data)
 
-# print(r_table)
-if len(r_table) == 1:
-    # print(r_table[0].text)
-    parsed_table = r_table[0]
-    rows = parsed_table.find('tr')
-    header_row = rows[0]
-    header_cols = header_row.find('th')
-    header_names = [x.text for x in header_cols]
-    table_data = []
-    for row in rows[1:]:
-        # print(row.text)
-        cols = row.find('td')
-        row_data = []
-        for i, col in enumerate(cols):
-            # print(i, col.text, '\n\n')
-            row_data.append(col.text)
-        table_data.append(row_data)
+    # print(header_names)
+    # print(table_data)
+    df = pd.DataFrame(table_data, columns=header_names)
+    path = os.path.join(BASE_DIR, 'data')
+    os.makedirs(path, exist_ok=True)
+    filepath = os.path.join('data', f'{name}.csv')
+    df.to_csv(filepath, index=False)
 
-print(header_names)
-print(table_data)
+
+def run(start_year=None, years_ago=10):
+    if start_year == None:
+        now = datetime.datetime.now()
+        start_year = now.year
+    assert isinstance(start_year, int)
+    assert len(f'{start_year}') == 4
+
+    url = f'https://www.boxofficemojo.com/year/world/{start_year}/'
+    parse_and_extract(url, name=start_year)
+
+
+if __name__ == '__main__':
+    run()
 
 # command: python3 -i scrape.py
